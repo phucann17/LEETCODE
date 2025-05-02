3f65f22f167abe67e73d30b66994e2379b7ac8a8
phucann17 <annnguyen2210022@gmail.com>
2025-05-02 20:12:00 +0700

    update

---
+++ Leetcode_75/1071_ Greatest_Common_Divisor_of_Strings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+    public:
+        string gcdOfStrings(string str1, string str2) {
+            string res;
+            if (str1 + str2 != str2 + str1) return "";
+            int a = str1.size(), b = str2.size();
+            while (b != 0){
+                int c = b;
+                b = a % b;
+                a = c;
+            }
+            for (int i = 0; i < a; i++){
+                    res += str1[i];
+            }
+    
+            return res;
+        }
+    };
