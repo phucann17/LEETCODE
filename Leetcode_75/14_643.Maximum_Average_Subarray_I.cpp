c0bb3c75803437465bd05b65292bae4572ee43d0
phucann17 <annnguyen2210022@gmail.com>
2025-05-11 10:03:50 +0700

    Update ex14

--- Leetcode_75/14_643.Maximum_Average_Subarray_I.cpp
+++ Leetcode_75/14_643.Maximum_Average_Subarray_I.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class Solution {
+    public:
+        double findMaxAverage(vector<int>& nums, int k) {
+            double maxVal = -10e5, sum = 0;
+            for (int i = 0; i < k; i++){
+                sum += nums[i];
+            }
+            maxVal = max(maxVal, sum);
+            for (int i = k; i < nums.size(); i++){
+                sum = sum + nums[i] - nums[i - k];
+                maxVal = max(sum, maxVal);
+            }
+            return maxVal/k;
+        }
+    };
