7228707f4e0311212b4bf57eb840cc229ba96c05
phucann17 <annnguyen2210022@gmail.com>
2025-05-14 10:02:16 +0700

    Update ex21

--- Leetcode_75/21_1207.Unique_Number_of_Occurrences.cpp
+++ Leetcode_75/21_1207.Unique_Number_of_Occurrences.cpp
@@ -1,2 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
+
+class Solution {
+    public:
+        bool uniqueOccurrences(vector<int>& arr) {
+            sort(arr.begin(), arr.end());
+            vector<int> tmp;
+            
+            int cnt = 1, j = 0, i = 0;
+            while (i < arr.size()){
+                cnt = 1;
+                while (j + 1 < arr.size() && arr[j] == arr[j + 1]){
+                    cnt++;
+                    j++;
+                }
+                tmp.push_back(cnt);
+                j++;
+                i = j;
+            }
+            unordered_set<int> res(tmp.begin(), tmp.end());
+            return tmp.size() == res.size();
+        }
+    };
